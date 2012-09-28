f5ead8ce3b49fcda037058e7afe113333d263af0
Davide Bettio <davide.bettio@gmail.com>
2012-09-28 22:53:42 +0200

    Modified

--- unicorn_init.sh
+++ unicorn_init.sh
@@ -7,7 +7,8 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/example
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+UNICORN=`rbenv which unicorn`
+CMD="$UNICORN -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
 set -u
 
@@ -26,7 +27,7 @@ oldsig () {
 case $action in
 start)
         sig 0 && echo >&2 "Already running" && exit 0
-        su -c "$CMD" - vagrant
+        $CMD
         ;;
 stop)
         sig QUIT && exit 0
@@ -39,7 +40,7 @@ force-stop)
 restart|reload)
         sig HUP && echo reloaded OK && exit 0
         echo >&2 "Couldn't reload, starting '$CMD' instead"
-        su -c "$CMD" - vagrant
+        $CMD
         ;;
 upgrade)
         if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
@@ -59,7 +60,7 @@ upgrade)
                 exit 0
         fi
         echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-        su -c "$CMD" - vagrant
+        $CMD
         ;;
 reopen-logs)
         sig USR1
